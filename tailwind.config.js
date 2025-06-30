f7eb5031085939f4b593287a5f2c421585e7cc08
Iris-Vision <Iriseye42@gmail.com>
2025-06-30 14:33:18 +0300

    Navbar, Menu, text

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,18 +11,4 @@ module.exports = {
   },
   plugins: [],
 };
-module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/flowbite-react/**/*.js",
-    "./node_modules/flowbite/**/*.js",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    require('flowbite/plugin')
-  ],
-};
+
